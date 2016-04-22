c58acbcf55f5b97bc10cbf1e97870c238bcc485d
kaiueo <kaiueo@outlook.com>
2016-04-22 16:51:28 +0800

    Finish Moving

--- Tetris/Game.cs
+++ Tetris/Game.cs
@@ -8,5 +8,85 @@ namespace Tetris
 {
     class Game
     {
+        private Wall wall = new Wall();
+        private Brick currentBrick;
+        private int currentX;
+        private int currentY;
+        private int score;
+        public void changeState()
+        {
+            currentBrick.changeDirection();
+        }
+
+        private bool downable()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (currentBrick.CurrentShape[i][j] == 1)
+                    {
+                        if (i + currentY + 1 >= 20)
+                        {
+                            return false;
+                        }
+                        if (j + currentX >= 14)
+                        {
+                            currentX = 13 - j;
+                        }
+                        if (wall.Bg[i + currentY + 1, j + currentX] == 1)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        private bool lefeable()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (currentBrick.CurrentShape[i][j] == 1)
+                    {
+                        if (j + currentX - 1 < 0)
+                        {
+                            return false;
+                        }
+                        if (wall.Bg[i + currentY, j + currentX - 1] == 1)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        private bool rightable()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    if(currentBrick.CurrentShape[i][j] == 1)
+                    {
+                        if (j + currentX + 1 > 14)
+                        {
+                            return false;
+                        }
+                        if(wall.Bg[i+currentY, j + currentX + 1] == 1)
+                        {
+                            return false;
+                        }
+                    }
+
+                }
+            }
+            return true;
+        }
     }
 }
